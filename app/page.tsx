11c6a5b575375cb86c2997cd060996eb4202417e
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-07-15 16:04:54 -0400

    Adopt repository: app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="flex flex-col gap-2 text-lg">
+          <Link href="/version/">Version</Link>
+          <Link href="/basic">Basic</Link>
+          <Link href="/simple-query-runner">Simple Query Runner</Link>
+          <Link href="/dashboard/">Dashboard</Link>
+          <Link href="/query-editor/">Query Editor</Link>
+          <Link href="/multiply-by-two/">Multiply by Two (Worker Example)</Link>
+          <Link href="/worker/">Simple Query Runner inside a Worker</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
